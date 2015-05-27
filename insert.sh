e0615b0ca6b8b6eccb3f570127904fe279f71149
Steven <b01902044@ntu.edu.tw>
2015-05-27 22:36:14 +0800

    +update insert script to be recursive (for img/ under htdocs/)

--- insert.sh
+++ insert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # insert hypertext related files 
-sudo cp ./src/htdocs/* /var/www/html/webMS/
+sudo cp -r ./src/htdocs/* /var/www/html/webMS/
 
 # insert cgi-bin
 sudo cp ./src/cgi-bin/* /var/www/html/cgi-bin/
